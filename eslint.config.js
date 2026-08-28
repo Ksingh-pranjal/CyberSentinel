d248839fd0096acb632c6e81cba69ba74f273214
Dev Desai <desai.dev742@gmail.com>
2026-08-28 20:07:21 +0530

    deleted mistakenly commited node_modules

--- eslint.config.js
+++
@@ -1,1 +0,0 @@
-import js from '@eslint/js'; import reactHooks from 'eslint-plugin-react-hooks'; export default [{ignores:['dist']},js.configs.recommended,{files:['src/**/*.{ts,tsx}'],plugins:{'react-hooks':reactHooks},rules:{...reactHooks.configs.recommended.rules,'no-unused-vars':'off'}}];
